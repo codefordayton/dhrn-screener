01ed29b5a3ae3801e9063ed9ee8fc8a87087f24d
Dave Best <dave.e.best@gmail.com>
2024-01-20 21:02:53 -0500

    Splitting the survey into a separate file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,12 @@
 import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
+import survey from '../data/survey.json'
 
 import 'survey-core/defaultV2.min.css';
 
-const surveyJson = {
-  firstPageIsStarted: true,
-  startSurveyText: "Take the Survey",
-  pages: [{
-    elements: [{
-      type: "html",
-      html: "<h2>In this survey, we will ask you a couple questions about your impressions of our product.</h2>"
-    }],
-  },{
-    elements: [{
-      name: "FirstName",
-      title: "Enter your first name:",
-      type: "text"
-    }, {
-      name: "LastName",
-      title: "Enter your last name:",
-      type: "text"
-    }]
-  }]
-};
-
 export default function App() {
 
-  const survey = new Model(surveyJson);
+  const surveyConfig = new Model(survey);
 
-  return <Survey model={survey} />;
+  return <Survey model={surveyConfig} />;
 }
