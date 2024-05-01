9fb9f7fff4e0584b8b51b2888bc3a901d951a7ac
Dave Best <dave.e.best@gmail.com>
2024-05-01 16:35:16 -0400

    splitting out the survey into a separate component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,19 @@
-import { Model } from 'survey-core';
-import { Survey } from 'survey-react-ui';
-import survey from '../data/survey.json'
-import { FunctionFactory } from "survey-core";
-
-
-import 'survey-core/defaultV2.min.css';
-
-const addressEndpoint = 'https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-72f50c41-0f6d-47f1-a509-bfc5cbc49013/dhrn/addresses';
+import React from "react";
+import Screener from "./Screener"
 
 export default function App() {
 
-  const surveyConfig = new Model(survey);
+  [showSurvey, setShowSurvey] = React.useState(true);
 
-  function isSingleFamilyLUC(luc) {
-    if (luc >= 500 && luc < 515) return true;
-    return false;
+  const completeSurvey = (sender, options, addressData)  => {
+    console.log("COMPLETE", sender, options, addressData);
+    setShowSurvey(false);
   }
 
-  function populateFromAddress(addressData) {
-    if (!addressData) {
-      return;
-    }
-
-    surveyConfig.setValue('address', addressData.address);
-
-    if (isSingleFamilyLUC(addressData.luc)) {
-      surveyConfig.setValue('homeType', 'Single Family');
-    } else {
-      surveyConfig.setValue('homeType', 'Duplex or other Multi-Unit');
-    }
-
-    surveyConfig.setValue('taxesUpToDate', addressData.delqamount === '0');
-  } 
-
-  function validateAddress([address]) {
-    console.log("VALIDATE", address);
-    if (!address || address.length < 5) {
-      this.returnResult(false);
-    }
-    fetch(`${addressEndpoint}?address=` + address).then(response => {
-      if (response.ok) {
-        response.json().then(data => {
-          console.log("DATA", data)
-          if (data.length === 0) {
-            this.returnResult(true);
-          } else {
-            populateFromAddress(data[0]);
-            this.returnResult(true);
-          }
-        });
-      } else {
-        this.returnResult(true);
-        return;
-      }
-    }).catch(() => {
-      this.returnResult(true);
-      return;
-    });
-  }
-  FunctionFactory.Instance.register("validateAddress", validateAddress, true);
-  surveyConfig.onComplete.add(function (sender) {
-    console.log("COMPLETE", sender);
-  });
-  
-  return <Survey model={surveyConfig} />;
+  const ScreenerMemo = React.memo(Screener);
+  return (
+  <>
+    { showSurvey && <ScreenerMemo onComplete={completeSurvey} setAddressData={setAddressData}/>}
+    { !showSurvey && <div>Survey Complete</div>}
+  </>);
 }
---
+++ src/components/Screener.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+import survey from '../data/survey.json'
+import { FunctionFactory } from "survey-core";
+
+
+import 'survey-core/defaultV2.min.css';
+
+const addressEndpoint = 'https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-72f50c41-0f6d-47f1-a509-bfc5cbc49013/dhrn/addresses';
+
+export default function Screener({onComplete, setAddressData}) {
+  // Using a state variable with this causes the survey to re-render on every state change,
+  // but not checking for a change causes multiple checks with the api.
+  let lastAddress = null;
+  let lastAddressData = null;
+
+  const surveyConfig = new Model(survey);
+  surveyConfig.onComplete.add((sender, options) => onComplete(sender, options, lastAddressData));
+
+  function isSingleFamilyLUC(luc) {
+    if (luc >= 500 && luc < 515) return true;
+    return false;
+  }
+
+  function populateFromAddress(addressData) {
+    if (!addressData) {
+      return;
+    }
+    lastAddressData = addressData;
+    lastAddress = addressData.address;
+    surveyConfig.setValue("address", addressData.address);
+
+    if (isSingleFamilyLUC(addressData.luc)) {
+      surveyConfig.setValue("homeType", "Single Family");
+    } else {
+      surveyConfig.setValue("homeType", "Duplex or other Multi-Unit");
+    }
+
+    surveyConfig.setValue("taxesUpToDate", addressData.delqamount === "0");
+  }
+
+  function validateAddress([address]) {
+    console.log("VALIDATE", address);
+    if (address === lastAddress) {
+      this.returnResult(true);
+      return;
+    }
+
+    if (!address || address.length < 5) {
+      this.returnResult(false);
+    }
+    lastAddress = address;
+
+    fetch(`${addressEndpoint}?address=` + address)
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            console.log("DATA", data);
+            if (data.length === 0) {
+              this.returnResult(true);
+            } else {
+              populateFromAddress(data[0]);
+              this.returnResult(true);
+            }
+          });
+        } else {
+          this.returnResult(true);
+          return;
+        }
+      })
+      .catch(() => {
+        this.returnResult(true);
+        return;
+      });
+  }
+  FunctionFactory.Instance.register("validateAddress", validateAddress, true);
+
+  return <Survey model={surveyConfig} />;
+}
