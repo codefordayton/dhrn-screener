a66a1b61a21324894bcdf6897434cdf45995c920
Vinit Jain <jain.vinit14@gmail.com>
2024-07-02 19:55:12 -0400

    Update Results.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -11,9 +11,9 @@ import {
   rebuildingTogetherDaytonARPAProgram,
 } from "../utils/program";
 import { createCompletedHtml } from "../utils/survey/createCompletedHtml";
- 
-const sendmailEndpoint = "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-72f50c41-0f6d-47f1-a509-bfc5cbc49013/dhrn/sendmail"
-export default function Results({ surveyData, addressData, county }) {
+
+const sendmailEndpoint =
+  "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-72f50c41-0f6d-47f1-a509-bfc5cbc49013/dhrn/sendmail";
 export default function Results({ surveyData, addressData, county, goBack }) {
   console.log("Results", surveyData, addressData, county);
 
@@ -21,27 +21,24 @@ export default function Results({ surveyData, addressData, county, goBack }) {
     console.log("Survey", surveyData);
     const isEligibleForMiamiValleyCommunityActionPartnershipWeatherization =
       miamiValleyCommunityActionPartnershipWeatherization(surveyData);
-  
+
     const isEligibleForHabitatForHumanityEmergencyHomeRepair =
       habitatForHumanityEmergencyHomeRepair(surveyData);
-  
-    const isEligibleForCountyCorpHomeRepair = countyCorpHomeRepair(
-      surveyData
-    );
-  
+
+    const isEligibleForCountyCorpHomeRepair = countyCorpHomeRepair(surveyData);
+
     const isEligibleForMiamiValleyCommunityActionPartnershipEmergencyHomeRepair =
       miamiValleyCommunityActionPartnershipEmergencyHomeRepair(surveyData);
-  
-    const isEligibleForRebuildingTogetherDayton = rebuildingTogetherDayton(
-      surveyData
-    );
-  
+
+    const isEligibleForRebuildingTogetherDayton =
+      rebuildingTogetherDayton(surveyData);
+
     const isEligibleForHabitatForHumanityARPAProgram =
       habitatForHumanityARPAProgram(surveyData);
-  
+
     const isEligibleForRebuildingTogetherDaytonARPAProgram =
       rebuildingTogetherDaytonARPAProgram(surveyData);
-  
+
     return createCompletedHtml({
       isEligibleForCountyCorpHomeRepair,
       isEligibleForHabitatForHumanityARPAProgram,
